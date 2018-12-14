2bf869aca1b9bcc3e756c33a1f39013c258d8c11
brian_bowyer <BrianBowyer2015@u.northwestern.edu>
2018-12-14 15:13:10 -0600

    site setup

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_scss import Scss
+from flask_bootstrap import Bootstrap
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    Bootstrap(app)
+    Scss(app)
 
     @app.route('/')
     def index():
-        return "Hello World!"
+        return render_template('index.html')
+
+    @app.route('/about')
+    def about():
+        return render_template('about.html')
+
+    @app.route('/projects')
+    def projects():
+        return render_template('projects.html')
+
+    @app.route('/contact')
+    def contact():
+        return render_template('contact.html')
 
     return app
