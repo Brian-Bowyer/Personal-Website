d4514c5d75aaadeee3deaa17914069c61ca96e4a
brian_bowyer <BrianBowyer2015@u.northwestern.edu>
2018-12-20 16:40:51 -0600

    add contact form

--- website/__init__.py
+++ website/__init__.py
@@ -1,31 +1,39 @@
-from flask import Flask, render_template
+import os
+from collections import namedtuple
+
+from flask import Flask, render_template, redirect
 from flask_scss import Scss
 from flask_bootstrap import Bootstrap
-from collections import namedtuple
-Project = namedtuple('Project', ['filename', 'title', 'description'])
+from flask_wtf.csrf import CSRFProtect
 
+from .forms import ContactForm
+
+Project = namedtuple('Project', ['filename', 'title', 'description'])
 PROJECTS = [
-    Project('bagofnouns.jpg', 
-            "Bag of Nouns", 
-            "Web app to play the game Bag of Nouns (also sometimes called Monikers)"),
-    Project('bta.jpg', 
-            "Bubble Turtle Adventure", 
-            "Sidescrolling video game"),
+    #Project('bagofnouns.jpg', 
+    #        "Bag of Nouns", 
+    #        "Web app to play the game Bag of Nouns (also sometimes called Monikers)"),
+    #Project('bta.jpg', 
+    #        "Bubble Turtle Adventure", 
+    #        "Sidescrolling video game"),
     Project('blorb.png', 
             "Blorb", 
             "Procedurally generated resource-collecting video game with infinite map"),
-    Project('story_scraper.jpg', 
-            "Story Scraper", 
-            "Computational linguistics class projects which scraped large amounts of fanfiction to produce linguistic corpus"),
+    #Project('story_scraper.jpg', 
+    #        "Story Scraper", 
+    #        "Computational linguistics class project which scraped large amounts of fanfiction to produce linguistic corpus"),
     Project('self.jpg', 
             "This Very Website", 
             "This website! The one that you are reading right now.")
 ]
 
+
 def create_app(test_config=None):
     app = Flask(__name__)
     Bootstrap(app)
     Scss(app)
+    CSRFProtect(app)
+    app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
     @app.route('/')
     def index():
@@ -39,8 +47,15 @@ def create_app(test_config=None):
     def projects():
         return render_template('projects.html', PROJECTS=PROJECTS)
 
-    @app.route('/contact')
+    @app.route('/contact', methods=('GET', 'POST'))
     def contact():
-        return render_template('contact.html')
+        form = ContactForm()
+        if form.validate_on_submit():
+            return redirect('/success')
+        return render_template('contact.html', form=form)
+
+    @app.route('/success')
+    def success():
+        return "Success!"
 
     return app
