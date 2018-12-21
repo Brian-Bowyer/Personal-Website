d3cd518cee01c6878f606b75ed3bf5d353efbbeb
brian_bowyer <BrianBowyer2015@u.northwestern.edu>
2018-12-21 14:40:26 -0600

    replace index w/ about

--- website/__init__.py
+++ website/__init__.py
@@ -25,11 +25,11 @@ def create_app(test_config=None):
 
     @app.route('/')
     def index():
-        return render_template('index.html')
-
-    @app.route('/about')
-    def about():
         return render_template('about.html')
+
+    # @app.route('/about')
+    # def about():
+    #     return render_template('about.html')
 
     @app.route('/projects')
     def projects():
